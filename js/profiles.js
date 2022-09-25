fcea9956755373b2c5f362d93f8e3b73b51688af
Stanley Goodwin <67250149+ThermalDynamicality@users.noreply.github.com>
2022-09-25 14:34:27 -0400

    Initial Commit

---
+++ js/profiles.js
@@ -0,0 +1,457 @@
+const memberData = {
+	"eboardSort": [
+		"President",
+		"Vice President",
+		"Treasurer",
+		"Secretary"
+	],
+	"members": [
+		{
+			"name": "Damian Suarez",
+			"eboardRole": "President",
+			"major": "Mechanical Engineering",
+			"minor": "N/A",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/team/damian_suarez.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/team/damian_suarez_fun.jpg",
+					"size": "fun"
+				}
+			],
+			"bio": "I'm Damian Suarez, 1st year Mechanical engineering technology major AND president of HoGS? I'm from Miami, Florida (Yes its hot, Yes we have mosquitos, No I don't know Mr. Worldwide) I joined HoGS in 2021, and briefly served as project manager. If they're is any random electrical parts or wires on floor Probably from me. When I'm not in class I like to hanging and chilling around floor, or blowing myself up, its a 50 50. Ill leave you with some advice \"You can't do two illegal things at once\"."
+		},
+		{
+			"name": "Victoria Greever",
+			"eboardRole": "Vice President",
+			"major": "Biotechnology + Bioinformatics",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/team/victoria_greever.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/team/victoria_greever_fun.jpg",
+					"size": "fun"
+				}
+			],
+			"bio": "Hi! I'm Victoria Greever, a first-year biotechnology and molecular bioscience major from Weaverville, North Carolina. Before being elected to Vice-President, I was the PR Head and did random project things. When I'm not in class or working, you can usually find me hanging out on floor, drawing, or hiking when I can. Lastly, here is my biggest piece of advice to  new members: \"If Damian tells you its safe, grab the fire extinguisher and start recording for legal purposes.\""
+		},
+		{
+			"name": "Will Ebmeyer",
+			"eboardRole": "Treasurer",
+			"major": "Physics",
+			"minor": "Game Design",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/team/will_ebmeyer.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/team/will_ebmeyer_fun.jpg",
+					"size": "fun"
+				}
+			],
+			"bio": "Hello fellow humans!  I'm Will, a second-year physics major.  I'm also the 2020-2021 Vice President for HoGS and former Project Head.  I might be of few words, but don't let that scare you off!  I try to be helpful when possible, so if you need anything, let me know!\nLive long and prosper!"
+		},
+		{
+			"name": "Savannah Kidd",
+			"eboardRole": "Secretary",
+			"major": "Biochemistry",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/team/savannah_kidd.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/team/savannah_kidd_fun.jpg",
+					"size": "fun"
+				}
+			],
+			"bio": "Hi! My name is Savannah and I'm from Fairfield, Ohio, which is about 30 minutes from Cincinnati. I enjoy reading and will listen to just about any music, so recommendations are always appreciated. I also like a variety of TV shows and movies and love talking about them! I have 3 dogs back home and I miss them the most out of everyone, but I came so far from home for a reason! I love RIT and want to go into research one day and from what I've heard this is a great place to start!"
+		},
+		{
+			"name": "Ryan Lemole",
+			"major": "Software Engineering",
+			"enrollment": "Fourth Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/ryan_lemole.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_ryan_lemole.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I'm Ryan Lemole, 4th year software engineering student AND was president of HoGS! (Isn't that exciting?) I'm from Peabody, MA. I've been with HoGS since 2018, was PR Head, and CS Head so I've definitely been around. When I'm not in class or with the house I like longboarding around campus, listening to music WAY too loudly (my favorite artists are Aries and MGK) and messing around with lighting/sound/set design for theater productions. If you're thinking about joining, DO IT! You won't regret it!"
+		},
+		{
+			"name": "Sydney Tannenbaum",
+			"major": "Bioinformatics",
+			"enrollment": "Third Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/sydney_tannenbaum.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_sydney_tannenbaum.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Howdy! My name's Sydney, and I am a Bioinformatics major at RIT. I intend on graduating with a minor in Archaeological Sciences. In my free time, I enjoy playing Dungeons & Dragons. Feel free to message me about D&D!"
+		},
+		{
+			"name": "Jack Buell",
+			"major": "Mechanical Engineering",
+			"enrollment": "Third Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/jack_buell.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_jack_buell.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hey y'all, not much to say, I play games, and do too much homework. I'm always free to talk... if i'm actually free!"
+		},
+		{
+			"name": "Skyler Fox",
+			"major": "Electrical Engineering Technology",
+			"enrollment": "Third Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/skyler_fox.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_skyler_fox.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hello! We're Skyler, Wynn, and Storm! Yes its just one profile, but there's definitely still three of us. One body, 3 individuals. (Try not to take the biblical suggestion too far, we're not claiming anything like that xD) I'm Skyler, I'm the one you will probably talk to the most. Wynn's the fun one, you'll probably hear from her most in the discord. Storm only shows up once in a while, but they're alright too.\nWe all hail from the Southern Tier of NY, and I'm one of the former tech heads. We're happy to help with most tech problems, and vocal supporters of Free and Open Source Software. I also play Tekken (buff Lars btw), and love talking fighting games or computer things in general. When [HUMAN MALWARE] isn't a thing, we're normally active on floor, and can often be found in either the discord or the social lounge. Feel free to say hi!"
+		},
+		{
+			"name": "Andrew Wrightson",
+			"major": "Computer Science",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/andrew_wrightson.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_andrew_wrightson.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hello, I am Andrew. I always harbored a love for technology and anything related to computers. That being said I can talk about any topic. I have an unending to learn, which has lead me through a multitude of different topics and fields. I can talk all day about anything and everything. If there's anything that interests you that you would like to talk about, that you think I have not heard of before, I want to learn more about it. "
+		},
+		{
+			"name": "Sinclaire Ogof",
+			"major": "Public Policy",
+			"minor": "Environmental Studies",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/sinclaire_ogof.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_sinclaire_ogof.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hi everyone! My name is Sinclaire and I'm from the Middle-of-Nowhere Pennsylvania! I love science which is why I believe it needs to be more present in policy making (hence thy major). I am excited to be your historical head for this year and can't wait to see what RIT has to offer me!"
+		},
+		{
+			"name": "Riley Mott Williams",
+			"major": "environmental science",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/riley_mott_williams.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_riley_mott_williams.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I’m just some nerdy guy who loves spreadsheets, the environment, and programming! Reach out and talk to me about those things, my two cats, or whatever else’s on your mind. I dabble in videogames but not much oustide of Minecraft. Remember to stay safe, wear a mask, and vote.\n🏳️‍⚧️ 🐾 😷 🗳️ ❤️"
+		},
+		{
+			"name": "Alejandra Paige",
+			"major": "Biology",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/alejandra_paige.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_alejandra_paige.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Greetings! Congratulations for reading this far on the page! Are you exhausted yet? No? Just wait till you hit odysseys' bio below... Anyhoo! I am a Freshman to campus with third year status at RIT. I love talking, listening to music, and doing random research. I know everyone has said just how amazing HoGS is so I'm gonna change it up a bit. HoGS is a terrible place if you don't want to meet anyone, have no fun ever, and hate science. If that doesn't sound like you, join us in our quest to take over th.... Sorry not supposed to disclose that just yet... Join HoGS!!"
+		},
+		{
+			"name": "Brad Hanel",
+			"major": "Game Design and Development",
+			"minor": "French and Music Technology",
+			"enrollment": "Fourth Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/brad_hanel.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_brad_hanel.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hi! :) I'm Brad! I'm really into French culture and baseball, I play video games and do art and music (poorly), and I'm the president of Fowl Play, a comedy troupe on campus. I've been an Orientation Supervisor and an RA, so I know way more than anyone ever should about RIT. I used to be the Vice President of HoGS many moons ago.\n\nI hold the world record* for places lived at RIT, as I've lived in Ellingson, on HoGS, in Perkins Green, at the Province, at UC, and in Sol as an RA.\n\n*probably not a record"
+		},
+		{
+			"name": "Jackson O'Connor",
+			"major": "Computer Science",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/jackson_o_connor.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_jackson_o_connor.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hi, I'm Jackson O'Connor, a Computer Science major from Lancaster, New York. I love video games, talking about video games, the Bills(Love Josh Allen!), and coding! As you might be able to tell by my picture, I am incredibly cool, sick, awesome, and an absolute bro."
+		},
+		{
+			"name": "Zachary Ross",
+			"major": "Management Information Systems",
+			"minor": "Supply Chain Management",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/zachary_ross.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_zachary_ross.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hello there! my name is Zachary Ross. I was born and raised in South Florida. I'm a  man of many interests, enjoying games, literature, music and much more. I particularly like works of science fiction that place an emphasis on lore and high concepts. The notion of exploring new histories, cultures and ideas appeals to me greatly."
+		},
+		{
+			"name": "Josh Lindsey",
+			"major": "Game Design & Development",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/josh_lindsey.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_josh_lindsey.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Artist, gamer, night owl, and a passionate hater of country music"
+		},
+		{
+			"name": "Dwayne the Rock",
+			"major": "Door Holding",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/dwayne.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_dwayne.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hey there!  I'm Dwayne, the Rock, and I'm always happy to hold the Tech Lounge door open for you!"
+		},
+		{
+			"name": "Big Red",
+			"major": "Electrodynamics",
+			"enrollment": "First Year",
+			"photos": [
+				{
+					"URL": "images/portfolio/big_red.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/portfolio/modals/m_big_red.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hello!  I'm Big Red and I like long cables like myself to bring around angry pixies!"
+		}
+	]
+}
+
+let imageFailListener = {
+	type: "error",
+	once: true,
+	passive: true,
+	capture: false,
+	listener: (event) => {
+		if (event.target instanceof HTMLImageElement) {
+			event.target.setAttribute("src", "images/unknownProfile.svg");
+		} else {
+			throw "imageFail called on something other than an image";
+		}
+	}
+};
+
+processEboard: {
+	eboard = memberData.members.filter(member => member.eboardRole);
+	eboard.sort((a, b) => {
+		if (memberData.eboardSort.includes(a.eboardRole)) {
+			if (memberData.eboardSort.includes(b.eboardRole)) {
+				return memberData.eboardSort.indexOf(a.eboardRole) - memberData.eboardSort.indexOf(b.eboardRole);
+			} else {
+				return -1;
+			}
+		} else if (memberData.eboardSort.includes(b.eboardRole)) {
+			return +1;
+		} else {
+			return a.name.localeCompare(b.name);
+		}
+	});
+
+	eboard = eboard.map(member => new Elmen("div").withClasses("col-md-6").withChildren(
+		new Elmen("ul").withClasses("exemenu").withChildren(
+			new Elmen("li").withChildren(new Elmen("div").withClasses("exewrap").withChildren(
+				new Elmen("img").withClasses("img1").withAttributes({src: member.photos.find(photo => photo.size == "thumbnail").URL}).withListeners(imageFailListener),
+				new Elmen("img").withClasses("img2").withAttributes({src: member.photos.find(photo => photo.size == "fun").URL}).withListeners(imageFailListener)
+			)),
+			new Elmen("li").withClasses("exetext").withChildren(
+				new Elmen("h3").withChildren(member.name),
+				...([member.eboardRole, member.major, (member.minor ? member.minor : undefined), member.enrollment].map(infoLine =>
+					(infoLine === undefined) ? undefined : new Elmen("p").withChildren(infoLine)
+				))
+			)
+		),
+		new Elmen("p").withClasses("exedesc").withChildren(member.bio)
+	).done())
+
+	let eboardMarkup = [];
+	while (eboard.length > 0) {
+		if (eboard.length >= 2) {
+			eboardMarkup.push(new Elmen("div").withClasses("row").withChildren(eboard.shift(), eboard.shift()).done());
+		} else {
+			eboardMarkup.push(new Elmen("div").withClasses("row").withChildren(eboard.shift()).done());
+		}
+	}
+
+	document.addEventListener("DOMContentLoaded", () => {
+		let eboardSection = document.getElementById("eboard_members");
+		if (eboardSection) {
+			eboardMarkup.forEach(row => eboardSection.appendChild(row));
+		}
+	}, {passive: true, once: true, capture: false});
+}
+
+processMembers: {
+	members = memberData.members.filter(member => !member.eboardRole);
+	members.sort((a, b) => {
+		let comparison = (a.name ? a.name : "").localeCompare(b.name ? b.name : "");
+		if (comparison == 0) {
+			comparison = (a.major ? a.major : "").localeCompare(b.major ? b.major : "");
+		}
+		if (comparison == 0) {
+			comparison = (a.minor ? a.minor : "").localeCompare(b.minor ? b.minor : "");
+		}
+		if (comparison == 0) {
+			comparison = (a.enrollment ? a.enrollment : "").localeCompare(b.enrollment ? b.enrollment : "");
+		}
+		return comparison;
+	});
+	{
+		let id = 0;
+		members.forEach(member => member.id = id++);
+	}
+
+	gridItems = members.map(member =>
+		new Elmen("div").withClasses("bgrid", "folio-item").withChildren(
+			new Elmen("div").withClasses("item-wrap").withChildren(
+				new Elmen("a").withAttributes({href: `#modal-${member.id}`}).withChildren(
+					new Elmen("img").withAttributes({src: member.photos.find(photo => photo.size == "thumbnail").URL, alt: (member.name ? member.name : "")}).withListeners(imageFailListener),
+					new Elmen("div").withClasses("overlay").withChildren(
+						new Elmen("div").withClasses("portfolio-item-meta").withChildren(
+							new Elmen("h5").withChildren(member.name),
+							...([member.major, (member.minor ? member.minor : undefined), member.enrollment].map(infoLine =>
+								(infoLine === undefined) ? undefined : new Elmen("p").withChildren(infoLine)
+							))
+						)
+					),
+					new Elmen("div").withClasses("link-icon").withChildren(
+						new Elmen("i").withClasses("fa", "fa-plus")
+					)
+				)
+			)
+		)
+	);
+
+	modals = members.map(member =>
+		new Elmen("div").withClasses("popup-modal", "mfp-hide").withAttributes({id: `modal-${member.id}`}).withChildren(
+			new Elmen("div").withClasses("media").withChildren(
+				new Elmen("img").withAttributes({src: member.photos.find(photo => photo.size == "full").URL, alt: (member.name ? member.name : "")}).withListeners(imageFailListener)
+			),
+			new Elmen("div").withClasses("description-box").withChildren(
+				new Elmen("h4").withChildren(member.name),
+				new Elmen("p").withChildren(member.bio)
+			),
+			new Elmen("div").withClasses("link-box").withChildren(
+				new Elmen("a").withClasses("popup-modal-dismiss").withAttributes({href: "#"}).withChildren("Close")
+			)
+		)
+	);
+
+	document.addEventListener("DOMContentLoaded", () => {
+		let portfolioSection = document.getElementById("portfolio");
+		if (portfolioSection) {
+			portfolioSection.appendChild(
+				new Elmen("div").withClasses("row", "items").withChildren(
+					new Elmen("div").withClasses("bgrid-fourth", "s-bgrid-third", "tab-bgrid-half").withAttributes({id: "portfolio-wrapper"}).withChildren(
+						...gridItems
+					),
+					...modals
+				).done()
+			);
+		}
+
+		// from main.js:
+		$('.item-wrap a').magnificPopup({
+
+			 type:'inline',
+			 fixedContentPos: false,
+			 removalDelay: 300,
+			 showCloseBtn: false,
+			 mainClass: 'mfp-fade'
+
+		});
+
+		$(document).on('click', '.popup-modal-dismiss', function (e) {
+				e.preventDefault();
+				$.magnificPopup.close();
+		});
+	}, {passive: true, once: true, capture: false});
+}
