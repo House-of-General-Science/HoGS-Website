10d67327afee903b5900d251621d9aa792c946dd
Jacob Ziarniak <jaziar808@gmail.com>
2023-05-09 19:59:11 -0400

    Added new pictures, updated all profiles

--- js/profiles.js
+++ js/profiles.js
@@ -24,38 +24,37 @@ const memberData = {
 			],
 			"bio": "Hi! I'm Victoria Greever, a second-year biotechnology and molecular bioscience major and bioinformatics major from Weaverville, North Carolina. Before being elected to President, I was the Vice-President, PR Head and did random project things (if you need a safety proposal, I'm your person!). When I'm not in class or working, you can usually find me hanging out on floor, drawing, or hiking when I can. Lastly, here is my biggest piece of advice to new members: \"Don't be afraid to get involved! There are so many opportunities and you can make so many amazing friends.\" I'm always here if you need me, so reach out! :)"
 		},
-		{   "name": "Joseph Gysbers",
+		{   "name": "Penny Jankowski",
 			"eboardRole": "Vice President",
-			"major": "Mechanical Engineering",
-			"enrollment": "Second Year",
+			"major": "Chemical Engineering",
+			"enrollment": "First Year",
 			"photos": [
 				{
-					"URL": "images/members/joseph_gysbers.jpg",
+					"URL": "images/members/penny_jankowski.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/members/joseph_gysbers_fun.jpg",
+					"URL": "images/members/penny_jankowski_fun.jpg",
 					"size": "fun"
 				}
 			],
-			"bio": "Hello! I'm Joe, a second year mechanical engineering major from Newfane, New York. Before being elected Vice-President I used to be Housing Head, and I always help out with projects for Rochester Maker Faire and Imagine RIT. I'm usually hanging out on floor whenever I'm not busy, so if you need anything just let me know or feel free to stop by!"
+			"bio": "My name is Penny Jankowski and I'm a first year chemical engineering major from Cleveland, OH. Before becoming Vice President, I served as the mural committee head for HoGS, and I am also a member of the project committee. Aside from my interests in math and science, I enjoy painting and love music. I will not hesitate to recommend you a song (or multiple songs, or an album, or an artist's entire discography)."
 		},
-		{   "name": "Will Ebmeyer",
+		{   "name": "Jacob Ziarniak",
 			"eboardRole": "Treasurer",
-			"major": "Physics",
-			"minor": "Game Design & Math",
-			"enrollment": "Fourth Year",
+			"major": "Computer Science",
+			"enrollment": "First Year",
 			"photos": [
 				{
-					"URL": "images/members/will_ebmeyer.jpg",
+					"URL": "images/members/jacob_ziarniak.jpg",
 					"size": "thumbnail"
 				},
 				{
-					"URL": "images/members/will_ebmeyer_fun.jpg",
+					"URL": "images/members/jacob_ziarniak_fun.jpg",
 					"size": "fun"
 				}
 			],
-			"bio": "Hello fellow humans!  I'm Will, a fourth-year physics major.  I'm also the 2020-2022 VP, 2022 Treasurer for HoGS, and former Project Head.  I might be of few words, but don't let that scare you off!  I try to be helpful when possible, so if you need anything, let me know!\nLive long and prosper!"
+			"bio": "Hello! I'm Jacob Ziarniak, and I'm a first year computer science major from a small town near Buffalo, NY. While you're most likely to find me coding in the late hours of the night, I like to spend my free time running, playing games, listening to music (Rush is my favorite band), or obsessing over cars. Before becoming treasurer, I was head of the tech committee, and I love using my skills to work on projects. I always enjoy helping out when I can, so feel free to ask me for anything, anytime!"
 		},
 		{   "name": "Savannah Kidd",
 			"eboardRole": "Secretary",
@@ -122,8 +121,36 @@ const memberData = {
 			"bio": "Gimme your kneecaps."
 		},
 
-
-
+		{   "name": "Lucas Gordon",
+			"major": "Industrial Design",
+			"enrollment": "Third Year",
+			"photos": [
+				{
+					"URL": "images/members/lucas_gordon.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/lucas_gordon_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "I have always been a maker. I've been an avid LEGO fan since the age of 4, and I always gravitated toward activities with creative outlets. I have also always been interested in science and technology and finding ways to use them to solve practical problems. After a bit of turbulence, I ended up in Industrial/Product Design and am now in my third year of the program, enjoying taking ideas from the conceptual stage all the way to production."
+		},
+		{   "name": "Joseph Gysbers",
+			"major": "Mechanical Engineering",
+			"enrollment": "Second Year",
+			"photos": [
+				{
+					"URL": "images/members/joseph_gysbers.jpg",
+					"size": "thumbnail"
+				},
+				{
+					"URL": "images/members/joseph_gysbers_fun.jpg",
+					"size": "full"
+				}
+			],
+			"bio": "Hello! I'm Joe, a second year mechanical engineering major from Newfane, New York. I have served as both Vice-President and Housing Head, and I always help out with projects for Rochester Maker Faire and Imagine RIT. I'm usually hanging out on floor whenever I'm not busy, so if you need anything just let me know or feel free to stop by!"
+		},
 		{   "name": "Kevin Wu",
 			"major": "Computer Science",
 			"enrollment": "Second Year",
@@ -139,67 +166,6 @@ const memberData = {
 			],
 			"bio": "I'm from NYC. I love editing videos and coding. I'm responsible for some of the HoGS YouTube videos and server management for some of our games, mainly datapacks for one of our Minecraft servers. I love music composed by Hiroyuki Sawano, if anyone knows who that is."
 		},
-		{   "name": "Ryan Lemole",
-			"major": "Software Engineering",
-			"enrollment": "Fifth Year",
-			"photos": [
-				{
-					"URL": "images/members/ryan_lemole.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/ryan_lemole_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I'm Ryan Lemole, 5th year software engineering student AND was president of HoGS! (Isn't that exciting?) I'm from Peabody, MA. I've been with HoGS since 2018, was PR Head, and CS Head so I've definitely been around. When I'm not in class or with the house I like longboarding around campus, listening to music WAY too loudly (my favorite artists are Aries and MGK) and messing around with lighting/sound/set design for theater productions. If you're thinking about joining, DO IT! You won't regret it!"
-		},
-		{   "name": "Jack Buell",
-			"major": "Mechanical Engineering",
-			"enrollment": "Fourth Year",
-			"photos": [
-				{
-					"URL": "images/members/jack_buell.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/jack_buell_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Hey y'all, not much to say, I play games, and do too much homework. I'm always free to talk... if i'm actually free!"
-		},
-		{   "name": "Sydney Tannenbaum",
-			"major": "Bioinformatics",
-			"enrollment": "Fifth Year",
-			"photos": [
-				{
-					"URL": "images/members/sydney_tannenbaum.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/sydney_tannenbaum_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Howdy! My name's Sydney, and I am a Bioinformatics major at RIT. I intend on graduating with a minor in Archaeological Sciences. In my free time, I enjoy playing Dungeons & Dragons. Feel free to message me about D&D!"
-		},
-		{   "name": "Zachary Ross",
-			"major": "Management Information Systems",
-			"minor": "Supply Chain Management",
-			"enrollment": "Third Year",
-			"photos": [
-				{
-					"URL": "images/members/zachary_ross.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/zachary_ross_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Hello there! my name is Zachary Ross. I was born and raised in South Florida. I'm a  man of many interests, enjoying games, literature, music and much more. I particularly like works of science fiction that place an emphasis on lore and high concepts. The notion of exploring new histories, cultures and ideas appeals to me greatly."
-		},
 		{   "name": "Damian Suarez",
 			"major": "Mechanical Engineering Technology",
 			"enrollment": "Second Year",
@@ -213,7 +179,7 @@ const memberData = {
 					"size": "full"
 				}
 			],
-			"bio": "I'm Damian Suarez, 1st year Mechanical engineering technology major AND president of HoGS? I'm from Miami, Florida (Yes its hot, Yes we have mosquitos, Yes I know Mr. Worldwide) I joined HoGS in 2021, and briefly served as project manager. If they're is any random electrical parts or wires on floor Probably from me. When I'm not in class I like to hanging and chilling around floor, or blowing myself up, its a 50 50. Ill leave you with some advice \"You can't do two illegal things at once\"."
+			"bio": "I'm Damian Suarez, the man who makes things. I'm from Miami, Florida (Yes its hot, Yes we have mosquitos, Yes I know Mr. Worldwide) I joined HoGS in 2021, and briefly served as project manager. If they're is any random electrical parts or wires on floor Probably from me. When I'm not in class I like to hanging and chilling around floor, or blowing myself up, its a 50 50. Ill leave you with some advice \"You can't do two illegal things at once\"."
 		},
 		{   "name": "Leah Robinson",
 			"major": "Biotechnology & Molecular Bioscience",
@@ -262,7 +228,7 @@ const memberData = {
 		},
 		{   "name": "Anthony Patti",
 			"major": "Biochemistry",
-			"enrollment": "",
+			"enrollment": "First Year",
 			"photos": [
 				{
 					"URL": "images/members/anthony_patti.jpg",
@@ -275,40 +241,10 @@ const memberData = {
 			],
 			"bio": "I'm a first year biochem major from Long island New York."
 		},
-		{   "name": "Izzy Axford",
-			"major": "Biology",
-			"enrollment": "",
-			"photos": [
-				{
-					"URL": "images/members/izzy_axford.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/izzy_axford_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "Very good at procrastination."
-		},
-		{   "name": "Jacob Ziarniak",
-			"major": "Computing Exploration",
-			"enrollment": "",
-			"photos": [
-				{
-					"URL": "images/members/jacob_ziarniak.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/jacob_ziarniak_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I am a freshman this year at RIT, and I am from a small town close to Buffalo, NY. As a student studying in the field of computing, I do naturally enjoy math, reasoning, and problem solving. However, when I'm not up in the late hours of the night doing coursework, you'll often find me playing chess, speed-cubing, or just playing games online with friends. I like to run and play soccer as well, although I'll play almost any sport just for the fun of it. I also have an interest in cars and motorsport, and I hope that my future career in computing might land me a job somewhere in the automotive field."
-		},
 		{   "name": "Katie Miller",
 			"major": "Chemistry",
 			"minor": "Dance",
-			"enrollment": "",
+			"enrollment": "First Year",
 			"photos": [
 				{
 					"URL": "images/members/katie_miller.jpg",
@@ -324,7 +260,7 @@ const memberData = {
 		{   "name": "Nicolas Sovare",
 			"major": "Physics, Game Design & Development ",
 			"minor": "Math",
-			"enrollment": "",
+			"enrollment": "Second Year",
 			"photos": [
 				{
 					"URL": "images/members/nicolas_sovare.jpg",
@@ -335,11 +271,11 @@ const memberData = {
 					"size": "full"
 				}
 			],
-			"bio": "I'm a second year physics major from central New York. I've got 3 younger brothers that, unfortunately, do not also want to be physics majors. I also have an unhealthy obsession with video games."
+			"bio": "Physics Major at RIT. Everyone thinks I'm insane and they're probably right. I have three younger brothers who are, unfortunately, not physics majors at RIT"
 		},
 		{   "name": "Robert Vazquez",
 			"major": "Civil Engineering Technology",
-			"enrollment": "",
+			"enrollment": "Second Year",
 			"photos": [
 				{
 					"URL": "images/members/robert_vazquez.jpg",
@@ -350,26 +286,11 @@ const memberData = {
 					"size": "full"
 				}
 			],
-			"bio": "I'm a second year CVET student and I'm a third Dan."
-		},
-		{   "name": "Andrew Saridakis",
-			"major": "Computational Engineering",
-			"enrollment": "",
-			"photos": [
-				{
-					"URL": "images/members/andrew_saridakis.jpg",
-					"size": "thumbnail"
-				},
-				{
-					"URL": "images/members/andrew_saridakis_fun.jpg",
-					"size": "full"
-				}
-			],
-			"bio": "I'm from LI NY, Chinese-American student. I love swimming longboards and SCIENCE, with favoritism towards cooking science and biology."
+			"bio": "My name is Robert Anthony Vazquez. I am a second year Civil Engineering Technology (CVET) student. I'm a martial artist who's always interested in learning new things."
 		},
 		{   "name": "Benny Wang",
 			"major": "Applied Statistics",
-			"enrollment": "",
+			"enrollment": "First Year",
 			"photos": [
 				{
 					"URL": "images/members/benny_wang.jpg",
@@ -380,11 +301,11 @@ const memberData = {
 					"size": "full"
 				}
 			],
-			"bio": "A little about yourself. Benny's never gonna give up dadjoking you, and he's never gonna give up just brute forcing math problems. He's never gonna let you live down your most embarrassing moment, and he's never gonna run around campus playing Pokemon go. That's a lie, he'll desert you to go catch that gible. If it wasn't obvious, Benny loves his rickrolls. He plays valorant and pokemon go, and enjoys listening to Chinese music."
+			"bio": "Benny's never gonna give up dadjoking you, and he's never gonna give up just brute forcing math problems. He's never gonna let you live down your most embarrassing moment, and he's never gonna run around campus playing Pokemon go. That's a lie, he'll desert you to go catch that gible. If it wasn't obvious, Benny loves his rickrolls. He plays valorant and pokemon go, and enjoys listening to Chinese music."
 		},
 		{   "name": "Ryan Armitage",
 			"major": "Applied Mathematics",
-			"enrollment": "",
+			"enrollment": "First Year",
 			"photos": [
 				{
 					"URL": "images/members/ryan_armitage.jpg",
